cd6b3fd3fe25e15e1fe7c8d09b6a07e77f669813
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-09-12 12:18:37 +0600

    nav card bg

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -27,7 +27,7 @@ const ProductsPage = () => {
         <h1 className="text-3xl text-slate-700 font-bold text-center mb-8">Men's Products</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {menClothingItems.map((product) => (
-            <div key={product.id} className="product-card bg-[#f7cfe186] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
+            <div key={product.id} className="product-card bg-[#EBF6FC] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
               <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
                 <Image className="object-cover" src={product.imageUrl} alt={product.name} layout="fill" />
               </div>
@@ -60,7 +60,7 @@ const ProductsPage = () => {
         <h1 className="text-3xl font-bold text-center  text-slate-700 mb-8">Women's Products</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {womenClothingItems.map((product) => (
-            <div key={product.id} className="product-card  bg-[#f7cfe186] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
+            <div key={product.id} className="product-card  bg-[#EBF6FC] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
               <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
                 <Image className="object-cover" src={product.imageUrl} alt={product.name} layout="fill" />
               </div>
@@ -93,7 +93,7 @@ const ProductsPage = () => {
         <h1 className="text-3xl font-bold  text-slate-700 text-center mb-8">Kids' Products</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {kidsClothingItems.map((product) => (
-            <div key={product.id} className="product-card bg-[#f7cfe186] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
+            <div key={product.id} className="product-card bg-[#EBF6FC] shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
               <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
                 <Image className="object-cover" src={product.imageUrl} alt={product.name} layout="fill" />
               </div>
